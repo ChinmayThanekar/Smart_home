790f5d22a1d0638dcbc2e81235950769e379e337
ChinmayThanekar <84039458+ChinmayThanekar@users.noreply.github.com>
2026-04-09 14:32:08 +0530

    Change page title from 'SmartNest Automation' to 'Niva Novus'

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Page config
 st.set_page_config(
-    page_title="SmartNest Automation", 
+    page_title="Niva Novus", 
     page_icon="🏠",
     layout="wide",
     initial_sidebar_state="collapsed"
