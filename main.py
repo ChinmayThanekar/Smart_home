795ecf6cd6ebd87a867e14c12c1e800190df5ed1
ChinmayThanekar <84039458+ChinmayThanekar@users.noreply.github.com>
2026-04-10 20:05:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-logo_url = "https://github.com/ChinmayThanekar/Smart_home/blob/c3762dab5100a27dfa68cbe7450503a0dc2e51f3/Gemini_Generated_Image_uy80ituy80ituy80.png"
+logo_url = "https://raw.githubusercontent.com/ChinmayThanekar/Smart_home/blob/c3762dab5100a27dfa68cbe7450503a0dc2e51f3/Gemini_Generated_Image_uy80ituy80ituy80.png"
 # Page config
 st.set_page_config(
     page_title="Niva Novus", 
